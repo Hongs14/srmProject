a14148390256be3c017bb4cb62499cc10d9dd597
xogml9704 <xogml9704@naver.com>
2023-02-09 10:26:54 +0900

    암호화 변경

--- src/main/java/com/team01/webapp/HomeController.java
+++ src/main/java/com/team01/webapp/HomeController.java
@@ -2,12 +2,9 @@ package com.team01.webapp;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import com.team01.webapp.service.HomeService;
 
 import lombok.extern.log4j.Log4j2;
 
@@ -20,16 +17,10 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	@Autowired
-	private HomeService homeService;
-	
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home() {
 		log.info("정보 로그 실행");
-		
-		int a = homeService.insert();
-		System.out.println("값 : " + a);
 		
 		return "home";
 		
--- src/main/java/com/team01/webapp/dao/HomeDao.java
+++
@@ -1,9 +0,0 @@
-package com.team01.webapp.dao;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface HomeDao {
-	
-	public int insert();
-}
