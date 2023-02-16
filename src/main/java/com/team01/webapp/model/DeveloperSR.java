069f2186e2b601f1b18608891f97283131943218
xogml9704 <xogml9704@naver.com>
2023-02-16 17:39:36 +0900

    인적 자원 추가 리스트 완성

---
+++ src/main/java/com/team01/webapp/model/DeveloperSR.java
@@ -0,0 +1,12 @@
+package com.team01.webapp.model;
+
+import java.sql.Date;
+
+import lombok.Data;
+
+@Data
+public class DeveloperSR {
+	private String srTtl;
+	private Date hrStartDate;
+	private Date hrEndDate;
+}
--- src/main/java/com/team01/webapp/progress/dao/IProgressRepository.java
+++ src/main/java/com/team01/webapp/progress/dao/IProgressRepository.java
@@ -2,8 +2,10 @@ package com.team01.webapp.progress.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
+import com.team01.webapp.model.DeveloperSR;
 import com.team01.webapp.model.HR;
 import com.team01.webapp.model.ProgressDetail;
 import com.team01.webapp.model.SRFile;
@@ -12,6 +14,8 @@ import com.team01.webapp.model.SRType;
 import com.team01.webapp.model.SrProgressAjax;
 import com.team01.webapp.model.SrProgressList;
 import com.team01.webapp.model.System;
+import com.team01.webapp.model.Task;
+import com.team01.webapp.model.ThArr;
 
 @Repository
 public interface IProgressRepository {
@@ -26,4 +30,8 @@ public interface IProgressRepository {
 	List<SRFile> selectSrFileList(String srNo);
 	SRFile selectSrFile(int srFileNo);
 	List<HR> selectHumanResourceList(String srNo);
+	List<HR> selectDeveloperList(@Param("userDpNm") String userDpNm, @Param("srNo") String srNo);
+	List<DeveloperSR> selectDeveloperSR(int userNo);
+	List<Task> selectTaskList();
+	void insertDeveloper(ThArr thArr);
 }
