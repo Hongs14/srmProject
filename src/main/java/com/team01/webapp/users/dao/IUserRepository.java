3f9e64c1e64e712eb0d923927d01fa39f6f023eb
ryul5102 <ryul5102@icloud.com>
2023-02-20 16:04:20 +0900

    [#8] 요청 배치 수정

--- src/main/java/com/team01/webapp/users/dao/IUserRepository.java
+++ src/main/java/com/team01/webapp/users/dao/IUserRepository.java
@@ -7,8 +7,22 @@ import com.team01.webapp.users.model.User;
 @Mapper
 public interface IUserRepository {
 
+	/**
+	 * 유저 정보 가져오기
+	 * 
+	 * @author			김희률
+	 * @param userId	userId로 식별
+	 * @return			User 객체 리턴
+	 */
 	public User selectByUserId(String userId);
 
+	/**
+	 * 회원가입 처리
+	 * 
+	 * @author		김희률
+	 * @param user	User 객체 전달
+	 * @return		성공여부
+	 */
 	public int insert(User user);
 
 }
---
+++ src/main/webapp/resources/js/writeForm.js
@@ -0,0 +1,88 @@
+
+ //날짜 선택
+ $(document).ready(function () {
+	const today = new Date();
+	
+     // Bootstrap Date Picker
+     $('#simple-date1 .input-group.date').datepicker({
+       format: 'yyyy/mm/dd',
+       todayBtn: 'linked',
+       todayHighlight: true,
+       autoclose: true,        
+     });
+ });
+ 
+//오늘 날짜 디폴트로 입력
+$(document).ready(function() {
+	var todayResult = getTimeStamp();
+	console.log(todayResult);
+	document.getElementById('dateInput').value = todayResult;
+});
+	
+//오늘 날짜 양식
+function getTimeStamp() {
+  var d = new Date();
+
+  var s =
+    leadingZeros(d.getFullYear(), 4) + '/' +
+    leadingZeros(d.getMonth() + 1, 2) + '/' +
+    leadingZeros(d.getDate(), 2);
+
+  return s;
+}
+
+
+//오늘 날짜 양식 (+두자리)
+function leadingZeros(n, digits) {
+  var zero = '';
+  n = n.toString();
+
+  if (n.length < digits) {
+    for (i = 0; i < digits - n.length; i++)
+      zero += '0';
+  }
+  return zero + n;
+}
+
+ //파일 선택
+$(document).ready(function() {
+    $("#customFile").on('change',function(){
+        if(window.FileReader){
+        	var files = $(this)[0].files;
+            var filename = files[0].name + "<button type='button' onclick='function1()' class='btn btn-danger btn-sm m-1'  >X</button>";
+            
+            for(var i=1; i<files.length; i++){
+            	filename += "<br>" +
+            	 files[i].name +
+            	 "<a href='#' class='btn btn-danger btn-sm m-1' >X</a>" ;
+            }
+        } else {
+            var filename = $(this).val.split('/').pop().split('\\').pop();
+        }
+
+        //filename insert
+        $('#userfile').html(filename);
+    });
+}); 
+//파일 이름 출력 
+$(document).ready(function() {
+    $("#customFile").on('change',function(){
+    	let formData = new FormData();
+    	bet
+        if(window.FileReader){
+        	var files = $(this)[0].files;
+        	var filename;
+        	for(var i=0; i<files.length; i++){
+             	filename = files[i].name;
+             	console.log(filename);
+             	
+            }
+        } else {
+            var filename = $(this).val.split('/').pop().split('\\').pop();
+        }
+
+        //filename insert
+        $('#userfile').html(filename);
+    });
+});
+	 
