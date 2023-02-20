96e8937cb1781c56d45950fe82e39410f22334f3
xogml9704 <xogml9704@naver.com>
2023-02-20 16:02:29 +0900

    진척율 추가 뷰 완성

--- src/main/java/com/team01/webapp/progress/controller/ProgressController.java
+++ src/main/java/com/team01/webapp/progress/controller/ProgressController.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.FileCopyUtils;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -41,7 +42,15 @@ public class ProgressController {
 	@Autowired
 	private IProgressService progressService;
 	
-	// 리스트 필터링 값 불러오기
+	/**
+	 * 리스트 된 필터링 불러오기
+	 * 
+	 * @author					김태희
+	 * @param pageNo			클라이언트가 보낸 페이지 번호 정보 저장
+	 * @param progressfilter	클라이언트가 보낸 필터링 하기 위한 데이터 정보 저장
+	 * @param model				View로 데이터 전달을 위한 Model 객체 주입
+	 * @return					progress/list 로 return
+	 */
 	@RequestMapping(value="/progress/list/{pageNo}", method = RequestMethod.GET)
 	public String progressList(@PathVariable int pageNo, ProgressFilter progressfilter, Model model) {
 		
@@ -52,6 +61,14 @@ public class ProgressController {
 		return "progress/list";
 	}
 	
+	/**
+	 * 진척 관리 상세 정보 조회
+	 * 
+	 * @author				김태희
+	 * @param srNo			클라이언트가 보낸 SR 번호 정보 저장
+	 * @param model			View로 데이터 전달을 위한 Model 객체 주입
+	 * @return				progress/detail 로 return
+	 */
 	@RequestMapping(value="/progress/detail/{srNo}", method = RequestMethod.GET)
 	public String progressDetail(@PathVariable String srNo, Model model) {
 		
@@ -62,7 +79,16 @@ public class ProgressController {
 		return "progress/detail";
 	}
 	
-	// progress 리스트 ajax 안에 필터링된 리스트 불러오기
+	/**
+	 * progress 리스트 불러오기
+	 * 
+	 * @author					김태희
+	 * @param pageNo			클라이언트가 보낸 Page 번호 정보 저장
+	 * @param srProgressAjax	클라이언트가 보낸 필터링 후 데이터 정보 저장
+	 * @param model				View로 데이터 전달을 위한 Model 객체 주입
+	 * @param pager				클라이언트가 보낸 pager 데이터 정보 저장
+	 * @return					progress/progressListView 로 return
+	 */
 	@RequestMapping(value="progress/list/progressajax/{pageNo}", produces="application/json; charset=UTF-8")
 	public String progressAjax(@PathVariable String pageNo, @RequestBody SrProgressAjax srProgressAjax, Model model, Pager pager) {
 		
@@ -81,6 +107,15 @@ public class ProgressController {
 		return "progress/progressListView";
 	}
 	
+	/**
+	 * 상세 뷰에서 파일 다운로드
+	 * 
+	 * @author				김태희
+	 * @param srFileNo		클라이언트가 보낸 srFile 번호 정보 저장
+	 * @param userAgent		User-Agent header 정보 저장
+	 * @param response		HttpServletResponse 객체 주입
+	 * @throws Exception	예외 처리
+	 */
 	@RequestMapping(value="progress/detail/filedownload", method = RequestMethod.GET)
 	public void filedownload(int srFileNo, @RequestHeader("User-Agent") String userAgent, HttpServletResponse response) throws Exception {
 		SRFile srFile = progressService.getSrFile(srFileNo);
@@ -115,7 +150,14 @@ public class ProgressController {
 		}
 	}
 	
-	// Detail SR 인적 자원 list
+	/**
+	 * SR인적자원관리 리스트 불러오기
+	 * 
+	 * @author			김태희
+	 * @param hr		클라이언트가 보낸 hr 정보 저장
+	 * @param model		View로 데이터 전달을 위한 Model 객체 주입
+	 * @return			progress/humanResourceList 로 return
+	 */
 	@RequestMapping(value="progress/detail/progressajax/1", produces="application/json; charset=UTF-8")
 	public String humanResourceAjax(@RequestBody HR hr, Model model) {
 		
@@ -134,7 +176,14 @@ public class ProgressController {
 		return "progress/humanResourceList";
 	}
 	
-	// 개발자 추가
+	/**
+	 * SR인적자원 추가
+	 * 
+	 * @author			김태희
+	 * @param srNo		클라이언트가 보낸 sr 번호 정보 저장
+	 * @param thArr		클라이언트가 보낸 thArr 정보 저장
+	 * @return			progress/detail 로 리다이렉트
+	 */
 	@RequestMapping(value="progress/detail/developerinsert/{srNo}", produces="application/json; charset=UTF-8")
 	public String developerinsert(@PathVariable String srNo, @RequestBody ThArr thArr) {
 		
@@ -145,7 +194,14 @@ public class ProgressController {
 		return "redirect:/progress/detail/" + srNo;
 	}
 	
-	// 개발자 일정 수정
+	/**
+	 * SR인적자원 수정폼 불러오기
+	 * 
+	 * @author			김태희
+	 * @param hr		클라이언트가 보낸 hr 정보 저장
+	 * @param model		View로 데이터 전달을 위한 Model 객체 주입
+	 * @return			progress/humanResourceUpdateView 로 return
+	 */
 	@RequestMapping(value="progress/detail/developerUpdateView", produces="application/json; charset=UTF-8")
 	public String developerUpdateView(@RequestBody HR hr, Model model) {
 		Date sysdate = new Date();
@@ -162,6 +218,13 @@ public class ProgressController {
 		return "progress/humanResourceUpdateView";
 	}
 	
+	/**
+	 * SR인적자원 수정
+	 * 
+	 * @author		김태희
+	 * @param hr	클라이언트가 보낸 hr 정보 저장
+	 * @return		progress/detail 로 리다이렉트
+	 */
 	@RequestMapping(value="progress/detail/developerUpdate", produces="application/json; charset=UTF-8")
 	public String developerUpdate(@RequestBody HR hr) {
 		
@@ -170,7 +233,13 @@ public class ProgressController {
 		return "redirect:/progress/detail/" + hr.getSrNo();
 	}
 	
-	// 인적 자원 삭제
+	/**
+	 * SR인적자원 삭제
+	 * 
+	 * @author		김태희
+	 * @param hr	클라이언트가 보낸 hr 정보 저장
+	 * @return		progress/detail 로 리다이렉트
+	 */
 	@RequestMapping(value="progress/detail/developerDelete", produces="application/json; charset=UTF-8")
 	public String developerDelete(@RequestBody HR hr) {
 		
@@ -182,14 +251,31 @@ public class ProgressController {
 		return "redirect:/progress/detail/" + hr.getSrNo();
 	}
 	
-	
-	// Detail SR 진척율
+	/**
+	 * SR 진척율 리스트 불러오기
+	 * 
+	 * @author			김태희
+	 * @param hr		클라이언트가 보낸 hr 정보 저장
+	 * @param model		View로 데이터 전달을 위한 Model 객체 주입
+	 * @return			progress/progressRateList 로 return
+	 */
 	@RequestMapping(value="progress/detail/progressajax/2", produces="application/json; charset=UTF-8")
 	public String Progressrate(@RequestBody HR hr, Model model) {
 		
 		List<Progress> progressRateList = progressService.progressRateList(hr.getSrNo());
+		
 		model.addAttribute("progressRateList", progressRateList);
 		
 		return "progress/progressRateList";
+	}
+	
+	@RequestMapping(value="progress/detail/progressRateAdd/{progNo}", method=RequestMethod.POST)
+	public String ProgressRateAdd(@PathVariable int progNo, Model model) {
+		
+		Progress progress = progressService.progressRate(progNo);
+		
+		model.addAttribute("progress", progress);
+		
+		return "progress/progressRateAdd";
 	}
 }
