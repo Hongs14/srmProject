9db6fe3d5ee19c1dd02728477ca7ff350a3c2dfd
ryul5102 <ryul5102@icloud.com>
2023-03-02 08:56:29 +0900

    [#1]회원탈퇴

--- src/main/java/com/team01/webapp/users/controller/UserController.java
+++ src/main/java/com/team01/webapp/users/controller/UserController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -126,5 +127,11 @@ public class UserController {
 	public String myinfo() {
 		log.info("실행");
 		return "user/myInfo";
+	}
+	
+	@RequestMapping(value = "/user/unregister/{userNo}", method = RequestMethod.GET)
+	public String unregister(@PathVariable int userNo) {
+		log.info("실행");
+		return "redirect:/";
 	}
 }
